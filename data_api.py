fixed additional masking issues

data_api.py
@@ -36,9 +36,9 @@ def binary_display(a,title = ""):
         img.show(title)
 def display(a):
     misc.imshow(a)
-c = 0
+global_color_coord = 0
 def add_object(x,D='rects'):
-    global c
+    global global_color_coord
     if D=='rects':
         # adds a colored rectangle to p
         d = x.shape[0]
@@ -47,17 +47,16 @@ def add_object(x,D='rects'):
         miny,maxy,minx,maxx = min(corner1[1],corner2[1]),max(corner1[1],corner2[1]),min(corner1[0],corner2[0]),max(corner1[0],corner2[0])
         #c = rand(0,3) # color
         x[minx:maxx,miny:maxy] = 0
-        x[minx:maxx,miny:maxy,c] = 1
+        x[minx:maxx,miny:maxy,global_color_coord] = 1
         obj = np.zeros((d,d,3))
-        obj[minx:maxx,miny:maxy,c] = 1
-        c+=1
-        c%=3
+        obj[minx:maxx,miny:maxy,global_color_coord] = 1
+        #c+=1
+        #c%=3
         return obj
     else:
         return NotImplemented
 def get_background_mask(y):
     bg = np.ones(y[0].shape[:2])
-    pdb.set_trace()
     for mask in y:
         bg*=1-np.max(mask,2)
     return bg
@@ -67,18 +66,28 @@ def combine_masks(y):
     # output:
     #   - ret: (d^2)x(d^2) binary is-same-cluster matrix
     d = len(y[0])
-    ret = np.zeros((d**2,d**2))
-    for mask in y:
-        mask_vec = np.reshape(np.sum(mask,axis=2),(d**2,1))
-        ret += mask_vec*mask_vec.T
-    return ret
+    ret = np.ones((d**2,d**2))
+    latest = np.ones((d,d))
+    #mask_log,tmp_log,mask_vec_log = [],[],[]
+    i = 0
+    #pdb.set_trace()
+    for mask in y[::-1]:
+        tmp = np.sum(mask,axis=2)
+        tmp *= latest # remove previously seen pixels
+        mask_vec = np.reshape(tmp,(d**2,1))
+        #mask_log.append(mask),tmp_log.append(tmp),mask_vec_log.append(mask_vec)
+        i+=1
+        ret *= 1-mask_vec*mask_vec.T
+        latest *= 1-tmp
+    print ret
+    return 1-ret
 def get_img(n,k=1,d=28,exact_count=True):
     """
-    Returns n labeled data points
-    Start with rgb squares
+    Returns n images made of rgb squares
     label: segmentation mask
     k = avg num of obj. if exact_count then k == num objs
     """
+    global global_color_coord
     xs = np.zeros((n,d,d,3))
     ys = []
     for x in xs:
@@ -88,15 +97,26 @@ def get_img(n,k=1,d=28,exact_count=True):
         obj_ind=1
         i=1
         while obj_ind<num_objects+1:
+            #print '----- loop iter ------'
+            global_color_coord+=1
+            global_color_coord%=3
+            #print 'adding object of color:',global_color_coord
             new_mask = add_object(x)
-            new_mask_flat = np.sum(new_mask,axis=2)
-            masks *= (1-new_mask_flat)
-            new = (i)*new_mask_flat
-            masks+=new # book-keeping 
             y.append(new_mask)
             #print(np.unique(masks))
+
+            # book-keeping
+            new_mask_flat = np.sum(new_mask,axis=2)
+            masks *= (1-new_mask_flat) 
+            new = (i)*new_mask_flat
+            masks+=new
+            
             old = obj_ind
             obj_ind = len(np.unique(masks))
+            if not old<obj_ind:
+                #print 'nothing new'
+                global_color_coord-=1
+                global_color_coord%=3
             i+=1
         #y = combine_masks(y)
         ys.append(y)
@@ -201,7 +221,7 @@ def scatter_3d(x,indices=None):
     else:
         xs,ys,zs = x[:,0],x[:,1],x[:,2]
         ax.scatter(xs,ys,zs,marker='o')
-    #plt.show()
+    plt.show()
 def scatter(x,indices=None):
     d = x.shape[1]
     if d == 2:
@@ -242,7 +262,7 @@ def get_clst_mat(y,flag):
 def noisify(x):
     shape = x.shape
     #eps = 0.1
-    eps = 10
+    eps = .15
     noise = np.random.normal(0,eps,shape)
     return x+noise
 def flip_noisify(arr,flip_ratio=0.2):

main.py
@@ -5,7 +5,7 @@ from model import *
 import matplotlib.gridspec as gridspec
 import matplotlib.image as mpimg
 from tqdm import tqdm
-import datetime
+from datetime import datetime
 import pdb
 import inspect
 from tensorflow.python import debug as tf_debug
@@ -32,12 +32,14 @@ clusterer = None
 tf_clustering = None
 def init1():
     global embedder,clusterer,tf_clustering,data_params,k
-    k = 3 # num of rectangles to sample
-    d = 28 # img dim
+    #k = 3 # num of rectangles to sample
+    k = 3
+    #d = 28 # img dim
+    d = 40
     data_params = [d,3]
     embedder = ImgEmbedder(data_params)
-    #clusterer = GDKMeansClusterer([d**2,3],k+1,n_iters=150)
-    clusterer = EMClusterer([d**2,3],k+1,n_iters=50)
+    clusterer = GDKMeansClusterer2([d**2,3],k+1)
+    #clusterer = EMClusterer([d**2,3],k+1,n_iters=50)
     tf_clustering = clusterer.infer_clustering()
 def run1():
     global embedder,clusterer,tf_clustering,data_params,k
@@ -46,20 +48,38 @@ def run1():
     x,y = xs[0],ys[0] # (d,d,3) array , list of len==k
     bg = get_background_mask(y)
     bg_ = np.reshape(bg,(d**2,1))
-    y = combine_masks(y)
-    y+=bg_*bg_.T
+    #pdb.set_trace()
+    y_old = combine_masks(y)
+    y =y_old+bg_*bg_.T
+    #x = 10*x
+    x = noisify(x)
+    #x = 10*x
     x_new = sess.run(embedder.x_new,feed_dict={embedder.x:x})
+    #pdb.set_trace()
+    print('meow')
     x_new = noisify(x_new)
-    #clusterer = EMClusterer(x_new.shape,k+1)
-    clustering = sess.run(tf_clustering,feed_dict={clusterer.x:x_new})
-    last = clustering[-1]
-    #print 'Norm of difference:',np.linalg.norm(last-y)/(y.shape[0])**2
-    print 'Norm of difference:',np.linalg.norm(last-y)
-    plotup(clustering,x,y)
-def plotup(clustering,x,y,cost_log):
+    scatter_3d(x_new)
+    #plt.show()
+    pdb.set_trace()
+    #x_new = 30*x_new
+    #x_new = noisify(x_new)
+    #x_new = 10*x_new
+    #x_new = noisify(x_new)
+    #clusterer = GDKMeansClusterer2(data_params,k)
+    #tf_clustering = clusterer.infer_clustering()
+    tf_membership = clusterer.history_list
+    feed_dict = {clusterer.x:x_new}
+    [last_centroids,clustering_history,cost_history,membership_history,grad_log] = sess.run([clusterer.c,tf_clustering,clusterer.cost_log,clusterer.history_list,clusterer.grad_log],feed_dict=feed_dict)
+    #[clustering_history,membership_history] = sess.run([tf_clustering,tf_membership],feed_dict=feed_dict) # for em
+    #cost_history = []
+    last_cluster = clustering_history[-1]
+    last_membership = membership_history[-1]
+    more = [last_membership,last_centroids,cost_history]
+    plotup(clustering_history,x_new,y,more)
+def plotup(clustering,x,y,more):
     import matplotlib.animation as anim
     import types
-
+    last_membership,last_centroids,_ = more
     fig = plt.figure()
     fig.add_subplot(2,2,1)
     img_last = Image.fromarray(clustering[-1]*255)
@@ -77,18 +97,59 @@ def plotup(clustering,x,y,cost_log):
     img = Image.fromarray(y*255)
     plt.imshow(img)
     plt.title('ground truth clustering')
-    fig.add_subplot(2,2,4)
-    #plt.imshow(x)
-    plt.plot(cost_log)
-    plt.title('cost trajectory')
-    last = clustering[-1]
-    #print 'Norm of difference:',np.linalg.norm(last-y)/(y.shape[0])**2
-    print 'Norm of difference:',np.linalg.norm(last-y)
-    #ani.save('./animation.gif',writer='imagemagick',fps=30)
-    #fig.save('/tmp/fig.gif', writer='imagemagick', fps=30)
-    
-    plt.show()
+    ax = fig.add_subplot(2,2,4,projection='3d')
+    # show 3d scatter of data vectors:
     #pdb.set_trace()
+    indices = np.argmax(last_membership,1)
+    #indices = np.array(indices)
+    cs = ['r','b','g','k','c']
+    for i in range(4):
+        c = cs[i]
+        points = x[indices==i]
+        xs,ys,zs = list(points[:,0]),list(points[:,1]),list(points[:,2])
+        ax.scatter(xs,ys,zs,c=c,marker='o')
+    # plot centroid:
+    c=cs[4]
+    xs,ys,zs = list(last_centroids[:,0]),list(last_centroids[:,1]),list(last_centroids[:,2])
+    ax.scatter(xs,ys,zs,c=c,marker='o')
+    #plt.imshow(x)
+    plt.title('data')
+    #plt.xlim((-20,20))
+    #plt.ylim((-20,20))
+    #plt.zlim((-20,20))
+    #plt.plot(cost_log)
+    #plt.title('cost trajectory')
+    last_clustering = clustering[-1]
+    #print 'Avg. norm of difference:',np.linalg.norm(last-y)/(y.shape[0])**2
+    print 'Norm of difference:',np.linalg.norm(last_clustering-y)
+    print "Sanity check:",np.linalg.norm(last_clustering.T-last_clustering),np.linalg.norm(y.T-y)
+    ts = str(datetime.today())
+    #ani.save('./animation{}.gif'.format(ts),writer='imagemagick',fps=10)
+    #fig.save('/tmp/fig.gif', writer='imagemagick', fps=30)
+    print 'meow1'
+    """
+    pdb.set_trace()
+    for i in range(784):
+        if np.linalg.norm(y[i]-last_clustering[i])>1e-10:
+            print 'at disagreement'
+            print i,list(x[i]),last_centroids[indices[i]],indices[i]
+            #pdb.set_trace()
+    x_round = np.round(x)
+    for i in range(784):
+        for j in range(784):
+            norm = np.linalg.norm(x_round[i]-x_round[j])
+            sim = norm<0.5
+            same_clst = bool(y[i][j])
+            if sim!=same_clst:
+                print False,i,j
+    #    #print list(y[i])
+    #    #print list(last_clustering[i])
+    """
+    try:
+        plt.show()
+    except AttributeError:
+        pdb.set_trace()
+    print 'meow2'
 def run2():
     global embedder,clusterer,tf_clustering,data_params,k
     '''
@@ -151,20 +212,15 @@ def run2():
 
     pdb.set_trace()
 def run3():
-    # test the new gdkmeans clusterer
-    n,d = 10,3
-    data_params = n,d
-    k = 4
-    x = np.random.rand(n,d)
-    clusterer = GDKMeansClusterer2(data_params,k)
-    tf_clustering = clusterer.infer_clustering()
-    feed_dict = {clusterer.x:x}
-    sess.run(tf_clustering,feed_dict=feed_dict)
+    # check gradient flow through clusterers.
+    pass
 print('Starting TF Session')
 sess = tf.InteractiveSession()
 #sess = tf_debug.LocalCLIDebugWrapperSession(sess)
 #sess.add_tensor_filter("has_inf_or_nan", tf_debug.has_inf_or_nan)
-run2()
+init1()
+run1()
+print 'finish'
 """
 # plot approximately how good is each hypothesis.
 #from pylab import *

model.py
@@ -105,11 +105,12 @@ class GDKMeansClusterer1(BaseClusterer):
 
 class GDKMeansClusterer2(BaseClusterer):
     ''' optimize over cluster centroids '''
-    def __init__(self,data_params,k,n_iters=10):
-        self.learn_rate = .01
+    def __init__(self,data_params,k,n_iters=100):
+        self.learn_rate = .001
         self.curr_step = 0
         self.n_iters = n_iters
         self.k = k
+        GDKMeansClusterer2.k = k # todo: fix this
         self.n,self.d = tuple(data_params)
         self.x = tf.placeholder(tf.float32,[self.n,self.d]) # rows are data points
         self.init_params() #TF constants for inner-optimization
@@ -118,19 +119,19 @@ class GDKMeansClusterer2(BaseClusterer):
     def init_params(self):
         self.c = tf.random_normal([self.k,self.d],seed=2018) # centroid matrix
         #self.c = self.x[0],self.x[90],self.x[105],self.x[-1]
-        self.c = tf.Print(self.c,[self.c[i] for i in range(4)],"Init centroids:")
+        #self.c = tf.Print(self.c,[self.c[i] for i in range(4)],"Init centroids:")
         self.history_list = [] # different membership matrices across optimization
     def update_params(self): # overrides super class method
         self.curr_step+=1
-        for j in range(4):
-            self.c = tf.Print(self.c,[self.c[j]],"centroid{}:".format(str(j)))
+        #for j in range(4):
+        #    self.c = tf.Print(self.c,[self.c[j]],"centroid{}:".format(str(j)))
         self.c = self.clean_empty_centroids()
         cost = self.obj_f(self.c,self.x)
         self.cost_log.append(cost)
         grads = tf.gradients(cost,self.c)[0]
         grads = tf.Print(grads,[cost],"cost:")
-        for i in range(4):
-            grads = tf.Print(grads,[grads[i]],"grads[{}]:".format(str(i)))
+        #for i in range(4):
+        #    grads = tf.Print(grads,[grads[i]],"grads[{}]:".format(str(i)))
         grads = tf.Print(grads,[tf.reduce_sum(grads**2)],"grads_total:")
         #grads = tf.Print(grads,[cost],"cost:")
         self.grad_log+=[grads]
@@ -165,10 +166,10 @@ class GDKMeansClusterer2(BaseClusterer):
         x = self.x
         self.membership_mat = self.get_membership_matrix(c,x)
         self.cluster_sums = tf.reduce_sum(self.membership_mat,axis=0)
-        self.cluster_sums = tf.Print(self.cluster_sums,[self.cluster_sums[0],self.cluster_sums[1],self.cluster_sums[2],self.cluster_sums[3]],"Cluster sums:")
+        #self.cluster_sums = tf.Print(self.cluster_sums,[self.cluster_sums[0],self.cluster_sums[1],self.cluster_sums[2],self.cluster_sums[3]],"Cluster sums:")
         ths = tf.constant(0.5)
         clust_sums_clipped = tf.clip_by_value(self.cluster_sums,0,ths)
-        clust_sums_clipped = tf.Print(clust_sums_clipped,[clust_sums_clipped[0],clust_sums_clipped[1],clust_sums_clipped[2],clust_sums_clipped[3]],"clipped:")
+        #clust_sums_clipped = tf.Print(clust_sums_clipped,[clust_sums_clipped[0],clust_sums_clipped[1],clust_sums_clipped[2],clust_sums_clipped[3]],"clipped:")
         avg = tf.reduce_mean(clust_sums_clipped)
         bool_val = avg<ths # True iff one of the entries is below ths
         replace_indicator = tf.one_hot(tf.argmin(clust_sums_clipped),self.k)[tf.newaxis,:]
@@ -185,7 +186,8 @@ class GDKMeansClusterer2(BaseClusterer):
     @staticmethod
     def get_membership_matrix(c,x):
         # returns [n,k] tensor
-        k = 4 #todo
+        #k = 4 #Todo: fix this
+        k = GDKMeansClusterer2.k
         outer_subtraction= tf.subtract(x[:,:,None],tf.transpose(c),name='outer_subtraction') # [n,d,k]
         distance_mat = tf.reduce_sum(outer_subtraction**2,axis=1) # [n,k]
         distance_mat = tf.Print(distance_mat,[distance_mat[:5,:5]],"dist_mat:")
@@ -283,7 +285,7 @@ class ImgEmbedder(BaseEmbedder):
         x_embed = tf.reshape(x,[img_dim**2,num_col]) # [img_dim**2,num_col]
         x_embed = tf.Print(x_embed,[x_embed])
         x_embed = tf.identity(x_embed,name='x_embed')
-        return 100*x_embed
+        return x_embed
 class DeepSetEmbedder(BaseEmbedder):
     #rho1 = MLP([1])
     #rho2 = MLP([1])