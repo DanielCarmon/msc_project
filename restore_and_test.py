b063a85656e82934bebeb70854d85764f971b59a
Daniel Carmon <carmonda@mail.tau.ac.il>
2018-04-24 22:54:19 +0300

    deepset testing fixed

--- restore_and_test.py
+++ restore_and_test.py
@@ -17,40 +17,41 @@ import pickle
 from tensorflow.python import debug as tf_debug
 from sklearn.metrics import normalized_mutual_info_score as nmi
 
-# stuff which should be decided about:
-use_deepset = False # not implemented yet
-use_crop = False
-sess = tf.InteractiveSession()
-
-d = 299
 data_dir = '/specific/netapp5_2/gamir/carmonda/research/vision/caltech_birds'
 inception_weight_path = "/specific/netapp5_2/gamir/carmonda/research/vision/msc_project/inception-v3"
 project_dir = "/specific/netapp5_2/gamir/carmonda/research/vision/msc_project"
+
+use_deepset = False
+test_last = False 
+if test_last:
+    print 'Loading train data... '
+    data = load_specific_data(data_dir,range(101,201))
+    fname_prefix = '101_to_200_scores'
+else:
+    print 'Loading train data... '
+    data = load_specific_data(data_dir,range(1,101))
+    fname_prefix = '1_to_100_scores'
+sess = tf.InteractiveSession()
+
+d = 299
 embed_dim = 1001
 embedder = InceptionEmbedder(inception_weight_path,embed_dim=embed_dim)
 startpoint = tf.placeholder(tf.float32,[None,299,299,3])
 endpoint = embedder.embed(startpoint)
-'''
-if arg_dict['deepset']: # need to batchwise apply pointwise embedder
-    #startpoint = tf.placeholder(tf.float32,[None,299,299,3])
-    startpoint = model.x
-    #endpoint = embedder_pointwise.embed(startpoint)
-    endpoint = embedder_pointwise.out #?
-    pdb.set_trace()
-    print 'meow'
-'''
-sess.run(tf.global_variables_initializer())
-saver = tf.train.Saver(tf.global_variables())
+
 if use_deepset:
     embedder_pointwise = embedder
-    embedder = DeepSetEmbedder1(embed_dim).compose(embedder_pointwise) # Under Construction!
-# prepare test data
-print 'Loading train data... '
-first_100_data = load_specific_data(data_dir,range(1,101),use_crop=use_crop)
-print 'Loading test data... '
-last_100_data = load_specific_data(data_dir,range(101,201),use_crop=use_crop)
+    embedder = DeepSetEmbedder1(embed_dim)
+    n = data[0].shape[0]
+    shape = [n,embed_dim]
+    deepset_startpoint = tf.placeholder(tf.float32,shape=shape) 
+    deepset_endpoint = embedder.embed(deepset_startpoint)
+
+sess.run(tf.global_variables_initializer())
+saver = tf.train.Saver(tf.global_variables())
+
 KMeans = cluster.KMeans
-def test(test_data): 
+def test(test_data,use_deepset=False): 
     global startpoint,endpoint
     print 'begin test'
     test_xs,test_ys,test_ys_membership = test_data
@@ -69,7 +70,12 @@ def test(test_data):
         embedded_xs_batch = get_embedding(xs_batch,startpoint,endpoint)
         np_embeddings = np.vstack((np_embeddings,embedded_xs_batch))
         i+=1
-    np_embeddings_normalized = l2_normalize(np_embeddings)
+    if use_deepset:
+        global deepset_startpoint,deepset_endpoint
+        print 'before ds module:',np_embeddings
+        feed_dict = {deepset_startpoint: np_embeddings}
+        np_embeddings = sess.run(deepset_endpoint,feed_dict=feed_dict)
+        print 'after ds module:',np_embeddings
     # 2) cluster
     km = KMeans(n_clusters=100).fit(np_embeddings)
     labels = km.labels_
@@ -81,31 +87,32 @@ def test(test_data):
     return result
 
 ##range_checkpoints = range(151,600)  # get number of checkpoints to restore
-range_checkpoints = range(80)
+range_checkpoints = range(201,500)
 i_log = 100 # logging interval
 
 ##names = ['_em_5_iters','_tg_em_5_iters','_crop_em_5_iters','_curric_em_5_iters','_em_10_iters','_tg_em_10_iters','_crop_em_10_iters','_curric_em_10_iters']
-names = ['_lr_1e-5_tg_deepset_em_5_iters']
-#names = ['_fixed_rand_em_5_iters','_fixed_rand_em_10_iters']
+#names = ['_tg_deepset_xavier_init_em_5_iters']
+names = ['_kmeans++_init_em_5_iters','_kmeans++_init_em_10_iters']
 for name in names:
     results = []
-    test_last = False
-    if test_last:
-        data = last_100_data
-        cp_file_name = '101_to_200_scores{}.npy'.format(name)
+    cp_file_name = fname_prefix+'{}.npy'.format(name)
+    append_to_existing_log = range_checkpoints[0]!=0
+    print 'appending to existing log?',append_to_existing_log
+    if append_to_existing_log:
+        to_append = np.load(cp_file_name)
     else:
-        data = first_100_data
-        cp_file_name = '1_to_100_scores{}.npy'.format(name)
-    ##to_append = np.load(cp_file_name)
-    to_append = [[]]
+        to_append = [[]]
     for i in range_checkpoints:
         print 'testing for {}, checkpoint #{}. last 100?{}'.format(name,i,str(test_last))
         ckpt_path = project_dir+'/'+name+'/step_{}'.format(i_log*i)+'.ckpt'
-        saver.restore(sess,ckpt_path)
-        result = test(data)
-        results.append(result)
-        np.save(cp_file_name,[to_append[0]+results,name]) # append new results by copying prev and rewriting 
-        print 'checkpoint result:',result
+        try:
+            saver.restore(sess,ckpt_path)
+            result = test(data,use_deepset)
+            results.append(result)
+            np.save(cp_file_name,[to_append[0]+results,name]) # append new results by copying prev and rewriting 
+            print 'checkpoint result:',result
+        except:
+            pass
     print '*'*50
     print 'results for {}:'.format(name),results
     print '*'*50
