5142304b2bdc1dfa6c8e7f05913b6b85250c9ae0
Daniel Carmon <carmonda@mail.tau.ac.il>
2018-10-04 20:55:16 +0300

    moved mlp to embed.py

--- embed.py
+++ embed.py
@@ -20,6 +20,39 @@ def my_batch_norm(batch_x):
     eps = 0.0001
     return (batch_x-batch_mean)/tf.sqrt(batch_var)
 
+class MLP():
+
+    def __init__(self,layer_widths,x=None,name='',initializer = tf.ones_initializer()):
+        # args:
+        # n_hidden: list of layer widths
+        # x: TF tensor
+        self.name = name
+        self.out = x # this will be changed at build. self.out <- next_layer(self.out)
+        self.layer_widths = layer_widths
+        self.activations = [x]
+        self.params = []
+        self.initializer = initializer
+        self.build()
+    
+    def build(self):
+        activation = tf.nn.relu
+        initializer = self.initializer
+        depth = len(self.layer_widths)
+        for i in range(depth-1):
+            #initializer = tf.contrib.layers.xavier_initializer()
+            weight_matrix = tf.get_variable("{}_DeepSetWeightMatrix{}".format(self.name,str(i)), [self.layer_widths[i],self.layer_widths[i+1]],initializer=initializer)
+            self.params.append(weight_matrix)
+            weight_matrix = tf.Print(weight_matrix,[weight_matrix],'weight_matrix:')
+            #weight_matrix = tf.Variable(np.eye(self.layer_widths[0])) #@debug
+            #weight_matrix = tf.constant(np.eye(self.layer_widths[0])) #@debug
+            weight_matrix = tf.cast(weight_matrix,tf.float32)
+            self.activations.append(self.out)
+            self.out = tf.matmul(self.out,weight_matrix)
+            #self.out = activation(self.out)
+    
+    def output(self):
+        return self.out
+
 class BaseEmbedder:
     def __init__(self):
         self.params = []
