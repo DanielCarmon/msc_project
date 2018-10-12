8602575b3fbdde0843cc6434ace14a6e79a6b3e0
Daniel Carmon <carmonda@mail.tau.ac.il>
2018-10-12 19:34:09 +0300

    added renewed files

---
+++ model.py
@@ -0,0 +1,126 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import pdb
+from tqdm import tqdm
+from functools import reduce
+from embed import *
+from cluster import *
+
+def nan_alarm(x):
+    return tf.Print(x, [tf.is_nan(tf.reduce_sum(x))], "{} Nan?".format(x.name))
+
+def get_clustering_matrices(y_assign_history):
+    # args:
+    #   - y_assign_history: [n_iters,n,k] tensor, who's [i,:,:]th element is a membership matrix inferred at i'th step of inner-optimization process.
+    # output:
+    #   - y_partition_history: [n_iters,n,n] tensor, who's [i,:,:]th element is a clustering matrix corresponding to bs[i,:,:]
+    y_partition_history = tf.einsum('tij,tkj->tik', y_assign_history, y_assign_history)  # thanks einstein
+    return y_partition_history
+
+def my_entropy(p_vals):
+    ''' args: [k,] shaped tensor with positive entries that sum to 1. '''
+    nats = tf.log(p_vals)
+    logs = nats/tf.log(2.)
+    return -tf.reduce_sum(p_vals*logs)
+
+def my_nmi(y_assign_gt,y_assign_predict):
+    ''' args: two row-stochastic matrices of shape [n,k]'''
+    n = tf.reduce_sum(y_assign_gt) # row-stochastic with n rows...
+    # get probabilities from assignments:
+    eps = 1e-3
+    joint_prob_mat = tf.matmul(y_assign_gt,y_assign_predict,transpose_a=True)/n # [k,k]
+    p_vals1 = tf.reduce_sum(y_assign_gt,axis=0) + eps
+    p_vals1 = p_vals1/tf.reduce_sum(p_vals1) # normalize
+    p_vals2 = tf.reduce_sum(y_assign_predict,axis=0) + eps
+    p_vals2 = p_vals2/tf.reduce_sum(p_vals2) # normalize
+    p_vals_joint = tf.reshape(joint_prob_mat,[-1]) + eps
+    p_vals_joint = p_vals_joint/tf.reduce_sum(p_vals_joint) # normalize
+    # calculate entropies:
+    entropy1 = my_entropy(p_vals1)
+    entropy1 = tf.Print(entropy1,[entropy1],'entropy_gt:')
+    entropy2 = my_entropy(p_vals2)
+    entropy2 = tf.Print(entropy2,[entropy2],'entropy_pred:')
+    entropy_joint = my_entropy(p_vals_joint)
+    entropy_joint = tf.Print(entropy_joint,[entropy_joint],'entropy_joint:')
+    # use mutual info identity:
+    mutual_info = entropy1+entropy2-entropy_joint
+    # return normalized: 
+    normalizer = tf.sqrt(entropy1*entropy2)
+    normalizer = tf.Print(normalizer,[normalizer],"normalizer: ")
+    normalizer *= 1.
+    return mutual_info/normalizer # sqrt is problematic. change to arithmetic avg?
+
+
+class Model:
+    optimizer_class = tf.train.AdamOptimizer
+    def __init__(self, data_params, embedder=None, clusterer=None, lr = 0.0001, is_img=False, sess=None, for_training=False, regularize=True, use_tg=True,obj='L2',log_grads=False):
+        self.sess = sess
+        self.embedder = embedder
+        self.clusterer = clusterer
+        self.n, self.d = tuple(data_params)
+        if is_img:
+            self.x = tf.placeholder(tf.float32, [None, self.d, self.d, 3])  # rows are data points
+        else:
+            self.x = tf.placeholder(tf.float32, [self.n, self.d])  # rows are data points
+        self.x = tf.cast(self.x, tf.float32)
+        #self.y = tf.placeholder(tf.float32, [self.n, self.n])
+        self.y = tf.placeholder(tf.float32, [None, None]) #[n,k]
+        self.y = tf.cast(self.y, tf.float32)
+        self.x_embed = self.embedder.embed(self.x,for_training) # embeddings tensor
+        ##self.x_embed = tf.Print(self.x_embed, [self.x_embed], "x_embed:", summarize=10)
+        self.lr = lr
+        self.optimizer = self.optimizer_class(lr)
+        self.clusterer.set_data(self.x_embed) # compose clusterer on top of embedder
+        self.clustering = self.clusterer.infer_clustering() # get output clustering
+        # compose loss func on top of output clustering:
+        if obj=='L2':
+            self.loss = self.L2_loss(self.clustering, self.y, use_tg)
+        elif obj=='nmi':
+            self.loss = self.NMI_loss(self.clustering, self.y, use_tg)
+        else:
+            print 'Error: Unsupported objective "',obj,'"'
+            exit()
+        if log_grads:
+            self.pre_grads = tf.gradients(self.loss, embedder.params) 
+            self.grads = filter((lambda x: x!=None),self.pre_grads) # remove None gradients from tf's batch norm params.
+            self.loss = tf.Print(self.loss,self.grads , 'gradient:')
+        else: 
+            self.grads = tf.constant(-1)
+            self.loss = tf.Print(self.loss,[self.loss], 'loss:')
+        #self.loss = tf.Print(self.loss, [self.loss], 'loss:')
+        
+        regularizer,beta = 0.,0.
+        if regularize:
+            for param in self.embedder.params:
+                print 'regularizing ',param
+                regularizer += tf.nn.l2_loss(param)
+        self.loss = beta*regularizer + self.loss
+        print 'building optimizer'
+        self.train_step = self.optimizer.minimize(self.loss)
+        print 'initializing global variables'
+        self.sess.run(tf.global_variables_initializer())
+        self.embedder.load_weights(self.sess)
+    @staticmethod
+    def L2_loss(y_pred, y, use_tg):
+        '''
+        args:
+            y_pred: [n_iters,n,k] tensor. history of soft assignments
+            y: [n,k] tensor. ground truth assignment
+            use_tg: bool. whether use trajectory ("aux") gradients or not
+        '''
+        y_pred = get_clustering_matrices(y_pred)
+        compare = y_pred[-1] # no trajectory gradient
+        if use_tg: # trajectory gradient
+            compare = y_pred
+        tensor_shape = tf.shape(compare)
+        normalize = tf.reduce_prod(tensor_shape) # num of entries
+        # normalize = 1.
+        # print 'gradient normalizing factor = ',normalize
+        y = tf.matmul(y,y,transpose_b=True)
+        return tf.reduce_sum((compare - y) ** 2) / tf.cast(normalize, tf.float32)
+    @staticmethod
+    def NMI_loss(y_pred,y,use_tg):
+        print 'in NMI_loss'
+        compare = y_pred[-1]# no support for tg yet
+        return -my_nmi(y,compare) # optimizer should minimize this
---
+++ restore_and_test.py
@@ -0,0 +1,189 @@
+import os
+import os.path
+import tensorflow as tf
+from sklearn import cluster
+import traceback
+import sys
+from control import dcdb
+from data_api import *
+from model import *
+import matplotlib.gridspec as gridspec
+import matplotlib.image as mpimg
+from tqdm import tqdm
+from datetime import datetime
+import pdb
+import inspect
+import pickle
+from tensorflow.python import debug as tf_debug
+from sklearn.metrics import normalized_mutual_info_score as nmi
+
+project_dir = "/specific/netapp5_2/gamir/carmonda/research/vision/new_embed"
+logfile_path = project_dir+'/log_test.txt'
+
+def log_print(*msg):
+    with open(logfile_path,'a+') as logfile:
+        msg = [str(m) for m in msg]
+        logfile.write(' '.join(msg))
+        logfile.write('\n')
+log_print('entered restore_and_test.py')
+def as_bool(s):
+    if s=='False': return False
+    if s=='True': return True
+
+def my_parser(argv):
+    ret = {} 
+    # default opts:
+    ret['use_tg'] = True # aux gradients
+    ret['obj'] = 'L2' # distance between pred and gt
+    ret['cluster'] = 'em' # cluster inference module
+    ret['cluster_hp'] = 1e-2 # bandwidth for em, step-size for km
+    ret['init'] = 2 # init method for clusterer
+    # override defaults:
+    n = len(argv)
+    for i in range(n):
+        if argv[i][:2]=="--": # is flag
+            key = argv[i][2:]
+            val_raw = argv[i+1]
+            try:
+                val = eval(val_raw)
+            except: # val should be string
+                val = val_raw 
+            ret[key]=val
+    # format opts:        
+    ret['permcovar'] = bool(ret['permcovar']) 
+    log_print('using options:',ret)
+    if ret['cluster'] == "em":
+        ret['cluster'] = EMClusterer
+    if ret['cluster'] == "kmeans":
+        ret['cluster'] = GDKMeansClusterer2
+    return ret
+
+argv = sys.argv
+arg_dict = my_parser(argv)
+inception_weight_path = "/specific/netapp5_2/gamir/carmonda/research/vision/new_embed/inception-v3"
+dataset_flag = arg_dict['dataset']
+use_permcovar = as_bool(arg_dict['permcovar'])
+eval_split = int(arg_dict['eval_split'])
+mini = eval_split>2
+name = arg_dict['name']
+log_print(name+': '+'Loading data... ')
+data = get_data(eval_split,dataset_flag)
+log_print(name+': '+'finished')
+split_name = ['train','test','valid','minitrain','minitest'][eval_split]
+fname_prefix = split_name+'_data_scores'
+gpu = arg_dict['gpu']
+os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu)
+config = tf.ConfigProto(allow_soft_placement=True)
+log_print(name+': '+'Starting TF Session')
+sess = tf.InteractiveSession(config=config)
+d = 299
+list_final_clusters = [100,98,512,102]
+n_final_clusters = list_final_clusters[dataset_flag]
+if mini: n_final_clusters = n_final_clusters/2
+embedder = InceptionEmbedder(inception_weight_path,new_layer_width=n_final_clusters)
+startpoint = tf.placeholder(tf.float32,[None,299,299,3])
+endpoint = embedder.embed(startpoint)
+
+if use_permcovar:
+    embedder_pointwise = embedder
+    embedder = PermCovarEmbedder1(n_final_clusters)
+    n = data[0].shape[0]
+    shape = [n,n_final_clusters]
+    permcovar_startpoint = tf.placeholder(tf.float32,shape=shape) 
+    permcovar_endpoint = embedder.embed(permcovar_startpoint)
+
+sess.run(tf.global_variables_initializer())
+saver = tf.train.Saver(tf.global_variables())
+
+KMeans = cluster.KMeans
+def test(test_data,use_permcovar=False): 
+    tic = dcdb.now()
+    global startpoint,endpoint
+    log_print(name+': '+'begin test')
+    test_xs,test_ys_membership = test_data
+    n_test = test_xs.shape[0]
+    n_clusters = test_ys_membership.shape[1]
+    # 1) embed batch by batch
+    def get_batch_iter(arr,bsz):
+        i = 0
+        n = arr.shape[0]
+        while bsz*i<n:
+            yield arr[bsz*i:bsz*(i+1)]
+            i+=1
+
+    def get_embedding(xs_batch,startpoint,endpoint):
+        feed_dict = {startpoint:xs_batch}
+        return sess.run(endpoint,feed_dict=feed_dict)
+    output_dim = int(endpoint.shape[1].__str__()) # width of last layer in embedder
+    
+    i=0
+    n_gpu_can_handle = 500
+    batch_iter = get_batch_iter(test_xs,n_gpu_can_handle)
+    embedding_list = []
+    while True:
+        try:
+            xs_batch = batch_iter.next()       
+            log_print(name+': '+'embedding batch ',i)
+            embedded_xs_batch = get_embedding(xs_batch,startpoint,endpoint)
+            embedding_list.append(embedded_xs_batch)
+            i+=1
+        except:
+            log_print(name+': '+'finished inception embedding')
+            np_embeddings = np.concatenate(embedding_list)
+            break
+    if use_permcovar:
+        global permcovar_startpoint,permcovar_endpoint
+        log_print(name+': '+'before ds module:',np_embeddings)
+        feed_dict = {permcovar_startpoint: np_embeddings}
+        np_embeddings = sess.run(permcovar_endpoint,feed_dict=feed_dict)
+        log_print(name+': '+'after ds module:',np_embeddings)
+    # 2) cluster
+    log_print(name+': '+'clustering ',np_embeddings.shape[0],'vectors to ',n_clusters,'clusters...')
+    km = KMeans(n_clusters=n_clusters).fit(np_embeddings)
+    log_print(name+': '+'finished clustering')
+    labels = km.labels_
+    #labels_normalized = km_normalized.labels_
+    nmi_score = nmi(labels, np.argmax(test_ys_membership, 1))
+    #nmi_score_normalized = nmi(labels_normalized, np.argmax(ys_membership, 1))
+    #scores = [nmi_score,nmi_score_normalized]
+    result = nmi_score
+    toc = dcdb.now()
+    log_print(name+': '+'elapsed: {}'.format(toc-tic))
+    return result
+    
+N = 6000
+if mini: N = 6000
+default_range_checkpoints = range(N) # might want to restore and test only a suffix of this
+i_log = 100 # logging interval
+results = []
+cp_file_name = project_dir+'/'+fname_prefix+'{}.npy'.format(name) 
+DIR = project_dir+'/{}'.format(name)
+try: # load previous results, see what checkpoint was last restored and tested
+    to_append = np.load(cp_file_name)[0]
+    n_tests_already_made = len(to_append)
+    range_checkpoints = default_range_checkpoints[n_tests_already_made:]
+    if n_tests_already_made == N:
+        log_print('{} evaluation completed'.format(name))
+    else:
+        log_print('restoring checkpoints in range',range_checkpoints[0],'to',range_checkpoints[-1])
+except:
+    log_print('no previous evaluations found. Evaluating from ckpt 0.')
+    range_checkpoints = default_range_checkpoints
+    to_append = []
+for i in range_checkpoints:
+    log_print('testing for {}, checkpoint #{}. data split:{}'.format(name,i,str(eval_split)))
+    ckpt_path = project_dir+'/'+name+'/step_{}'.format(i_log*i)+'.ckpt'
+    if use_permcovar: log_print('WARNING: using permcovar' )
+    #ckpt_path = '/specific/netapp5_2/gamir/carmonda/research/vision/new_embed/inception-v3/model.ckpt-157585' # for debug.
+    try:
+        saver.restore(sess,ckpt_path)
+    except:
+        log_print(name+': restore from '+ckpt_path+' failed. exiting program')
+        exit(0)
+    result = test(data,use_permcovar)
+    results.append(result)
+    np.save(cp_file_name,[to_append+results,name]) # append new results by copying prev and rewriting 
+    log_print(name+': '+'checkpoint result:',result)
+log_print('*'*50)
+log_print('results for {}:'.format(name),results)
+log_print('*'*50)
