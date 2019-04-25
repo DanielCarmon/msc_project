f91d8acbc7937a3498a8b9e1e67984e00c070716
Daniel Carmon <carmonda@mail.tau.ac.il>
2019-04-25 22:38:24 +0300

    new control scripts

--- control/get_workers.sh
+++ control/get_workers.sh
@@ -1,7 +1,7 @@
 #!/bin/tcsh
 set script_dir = /specific/netapp5_2/gamir/carmonda/research/vision/msc_project/control
 #set all_machines = ( rack-gamir-g01 savant rack-nachum-g01)
-set all_machines = ( pc-gamir-g01 pc-wolf-g02 rack-gamir-g01 rack-gamir-g02 )
+set all_machines = ( pc-gamir-g01 pc-wolf-g01 pc-wolf-g02 rack-gamir-g01 rack-gamir-g02 rack-nachum-g01 savant )
 #set all_machines = ( 'savant' )
 rm ~/workers.txt # delete old file
 foreach m ( $all_machines )
